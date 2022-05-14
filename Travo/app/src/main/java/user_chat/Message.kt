8ec8e2b7cbe1bbc56e1e5c4a549ca396847873c8
IshaniA505 <101962150+IshaniA505@users.noreply.github.com>
2022-05-14 23:25:19 +0530

    message class updated

--- Travo/app/src/main/java/user_chat/Message.kt
+++ Travo/app/src/main/java/user_chat/Message.kt
@@ -3,6 +3,7 @@ package user_chat
 import com.google.firebase.Timestamp
 import com.google.firebase.firestore.FieldValue
 
+//initializing the class Message
 class Message {
     var message: String? = null
     var senderId: String? = null
