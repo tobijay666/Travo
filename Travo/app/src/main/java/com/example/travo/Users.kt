0602497bce4b8cc044a0c9b6d111259742948427
Menaka Anton <88192133+tobijay666@users.noreply.github.com>
2022-05-14 23:32:34 +0530

    Add files via upload

---
+++ Travo/app/src/main/java/com/example/travo/Users.kt
@@ -0,0 +1,7 @@
+package com.example.travo
+
+import com.google.firebase.firestore.Exclude
+
+data class Users (var name:String?=null,var email:String?=null,var gem:String?=null,var image:String?=null)
+
+
