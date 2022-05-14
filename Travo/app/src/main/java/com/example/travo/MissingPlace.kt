5a1fbeb5a90a412de20d872382109bbacf3f5369
Yelani Jayasooriya <104297345+YelaJ@users.noreply.github.com>
2022-05-14 22:45:01 +0530

    adding places & changing forgotten passwords

---
+++ Travo/app/src/main/java/com/example/travo/MissingPlace.kt
@@ -0,0 +1,3 @@
+package com.example.travo
+
+data class MissingPlace(var userId:String?=null,var placeName:String?=null,var Address:String?=null,var category:String?=null,var number:String?=null,var status:Boolean?=false)
