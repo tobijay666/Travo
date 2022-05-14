8fcab383505cf790b8e6209677afe20c33bab484
Menaka Anton <88192133+tobijay666@users.noreply.github.com>
2022-05-14 23:31:10 +0530

    maniACT Updates

--- Travo/app/src/main/java/com/example/travo/MainActivity.kt
+++ Travo/app/src/main/java/com/example/travo/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.travo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private val locationFrag=LocationFrag()
+    private val allPlaceFrag=AllPlaceFrag()
+    private val profileFrag=ProfileFrag()
+    private val chatFrag=ChatFrag()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        replaceFragment(allPlaceFrag)
+
+        val bottomNavigationView : BottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        bottomNavigationView.setOnItemSelectedListener {
+            // do stuff
+            when(it.itemId){
+                R.id.dashboard->replaceFragment(allPlaceFrag)
+                R.id.location->replaceFragment(locationFrag)
+                R.id.chat->replaceFragment(chatFrag)
+                R.id.profile->replaceFragment(profileFrag)
+
+            }
+            return@setOnItemSelectedListener true
+        }
+
+    }
+    private fun replaceFragment(fragment: Fragment) {
+        val transaction=supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragContainer,fragment)
+        transaction.commit()
     }
 }
